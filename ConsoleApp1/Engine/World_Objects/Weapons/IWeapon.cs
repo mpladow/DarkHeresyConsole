2b4fdab8c8abd7a8288169ed6f4d57c1e65ab57e
Michael La Dow <mike@trunked.com.au>
2018-07-04 00:06:43 +1000

    commit weapon interface

---
+++ ConsoleApp1/Engine/World_Objects/Weapons/IWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.World_Objects.Weapons
+{
+    interface IWeapon
+    {
+        int CalculateDamage(int dicetoroll);
+
+        void EquipWeapon();
+
+        void ReadyWeapon();
+
+
+    }
+}
